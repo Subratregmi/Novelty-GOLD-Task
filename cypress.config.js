bb01887b1b74db81bf6b6b6f2fb4cbb0ddebfde9
subrat.regmi <subrat.regmi@amniltech.com>
2023-06-28 10:35:50 +0545

    mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     defaultCommandTimeout: 35000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 })
